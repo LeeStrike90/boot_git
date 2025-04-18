72f312f0769ab0f9b508262515cf4fbbe6611634
LEE <69142830+LeeStrike90@users.noreply.github.com>
2025-04-18 14:12:26 +0900

    33

--- src/main/java/com/boot/ExamController.java
+++ src/main/java/com/boot/ExamController.java
@@ -1,18 +1,16 @@
 package com.boot;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import lombok.extern.slf4j.Slf4j;
-
 @Controller
 @Slf4j
-public class ExamController {
+public class ExamController
+{
 	@RequestMapping("/")
-//	public void home() {
-	public String home() {
-		log.info("hello boot111 222");
-		
+//	public void home() { 
+	public String home()
+	{
+		log.info("hello boot111 222 333");
 		return "hello";
 	}
 }
